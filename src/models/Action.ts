8d723d9e56872e937b26a4138b8a1a5f2a6987df
Emad Jumaah <emadjumaah@gmail.com>
2021-10-05 17:07:33 +0300

    before start reminder sms

--- src/models/Action.ts
+++ src/models/Action.ts
@@ -6,14 +6,21 @@ const ActionSchema = new mongoose.Schema(
     autoNo: { type: Number, unique: true },
     indx: { type: Number },
 
+    id: { type: Number },
     type: { type: Number }, // 1: sms, 2: email, 3: notification
-    active: { type: Boolean, default: true },
+    title: { type: String },
+    body: { type: String },
+
     sendtime: { type: Date, index: true },
+    active: { type: Boolean, default: true },
+
+    rRule: { type: String },
+    exDate: { type: String },
+    allDay: { type: Boolean },
+
     phone: { type: String }, // for type = 1
     email: { type: String }, // for type = 2
     userId: { type: String }, // for type = 3
-    title: { type: String },
-    body: { type: String },
 
     timeunit: { type: String },
     timerelate: { type: String },
